ecc780e199b6443c82fc794feeb63d0e0297cc9f
S.M.ABTAHI NOOR <73340940+19smabtahinoor@users.noreply.github.com>
2021-06-21 00:59:57 +0600

    Version 1.7
    User authentication done

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import { Button } from '@material-ui/core'
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Header() {
     const [changeHeader, setChangeHeader] = useState(false)
+    const { loginWithRedirect,logout,user,isAuthenticated } = useAuth0();
 
     const handleHeader = () => {
         if (window.scrollY >= 80) {
@@ -15,6 +17,8 @@ function Header() {
         }
     }
     window.addEventListener("scroll", handleHeader)
+    const userInfo = JSON.stringify(user,null,2)
+    console.log(userInfo)
     return (
         <>
             <Navbar expand="lg" className={changeHeader ? "activeHeader header" : "header"}>
@@ -24,18 +28,48 @@ function Header() {
                     </div>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <div className="menus flex flex-col  mr-2 md:flex-row lg:flex-row xl:flex-row space-x-3">
-                                <Nav.Link className="menuList text-white font-normal text-md cursor-pointer font-semibold">About Us</Nav.Link>
-                                <Nav.Link className="menuList text-white font-normal text-md cursor-pointer font-semibold">Success</Nav.Link>
-                                <Nav.Link className="cursor-pointer"><img src="./assets/shoppingCart.png" alt="cart" /></Nav.Link>
-                            </div>
 
-                            <div className="header_buttons flex flex-row  flex-grow items-center space-x-3">
-                                <Button className="login_btn transition duration-500 ease-in-out rounded-full border border-gray-300 text-white px-4 py-2 text-center cursor-pointer font-semibold">Login</Button>
-                                <Button className="header_btn rounded-full text-white px-4 py-2 text-center cursor-pointer border border-gray-300 font-semibold">Register</Button>
-                            </div>
-                        </Nav>
+
+                      
+                            {isAuthenticated ? (
+                                <Nav className="ml-auto">
+                                <div className="menus flex flex-col  mr-2 md:flex-row lg:flex-row xl:flex-row space-x-3">
+                                    <Nav.Link className="menuList text-white font-normal text-md cursor-pointer font-semibold">My Classes</Nav.Link>
+                                    <Nav.Link className="cursor-pointer"><img src="./assets/notificationbell.png" alt="notification" /></Nav.Link>
+                                    <Nav.Link className="cursor-pointer"><img src="./assets/shoppingCart.png" alt="cart" /></Nav.Link>
+                                </div>
+    
+                                <div className="header_buttons flex flex-row  flex-grow items-center space-x-3">
+                                    <div className="">
+                                        <img className="rounded-full" src={user.picture} alt={user.name} width="40px" height="40px"/>
+                                    </div>
+                                <Button className="login_btn transition duration-500 ease-in-out rounded-full border border-gray-300 text-white px-4 py-2 text-center cursor-pointer font-semibold"
+                                onClick={() => logout()}>Logout</Button>
+                                </div>
+                                </Nav>
+                            ): (
+                                <Nav className="ml-auto">
+                                <div className="menus flex flex-col  mr-2 md:flex-row lg:flex-row xl:flex-row space-x-3">
+                                    <Nav.Link className="menuList text-white font-normal text-md cursor-pointer font-semibold">About Us</Nav.Link>
+                                    <Nav.Link className="menuList text-white font-normal text-md cursor-pointer font-semibold">Success</Nav.Link>
+                                    <Nav.Link className="cursor-pointer"><img src="./assets/shoppingCart.png" alt="cart" /></Nav.Link>
+                                </div>    
+                           
+                                <div className="header_buttons flex flex-row  flex-grow items-center space-x-3">
+                                <Button className="login_btn transition duration-500 ease-in-out rounded-full border border-gray-300 text-white px-4 py-2 text-center cursor-pointer font-semibold"
+                                onClick={() => loginWithRedirect()}>Login</Button>
+                                 <Button className="header_btn rounded-full text-white px-4 py-2 text-center cursor-pointer border border-gray-300 font-semibold"onClick={() => loginWithRedirect()}>Register
+                                </Button>
+                                </div>
+                                </Nav>
+                            )}
+                            
+
+                            
+                       
+
+
+
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
